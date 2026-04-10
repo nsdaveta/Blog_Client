b4d32acf8d248bab13e746aea44a71a6b24dd470
nsdaveta <100615660+nsdaveta@users.noreply.github.com>
2026-04-10 13:57:14 +0530

    Sync before build (automated)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,6 +12,7 @@ pub fn run() {
       Ok(())
     })
     .plugin(tauri_plugin_dialog::init())
+    .plugin(tauri_plugin_opener::init())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
