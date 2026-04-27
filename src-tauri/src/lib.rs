237d21f00f6bdebfe44be9a69bc05470feacbce0
nsdaveta <100615660+nsdaveta@users.noreply.github.com>
2026-04-27 09:30:11 +0530

    Sync before build (automated)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,28 +1,29 @@
 use tauri::Manager;
+
+#[cfg(target_os = "windows")]
 use window_vibrancy::apply_mica;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      let window = app.get_webview_window("main").unwrap();
+    tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(target_os = "windows")]
+            {
+                let window = app.get_webview_window("main").unwrap();
+                let _ = apply_mica(&window, None);
+            }
 
-      #[cfg(target_os = "windows")]
-      {
-        let _ = apply_mica(&window, None);
-      }
-
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .plugin(tauri_plugin_dialog::init())
-    .plugin(tauri_plugin_opener::init())
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_opener::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
